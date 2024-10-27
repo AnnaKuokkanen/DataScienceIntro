9db98ca3140690b8b93b7685bcbc8d510a6a6b49
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2024-10-27 11:42:20 +0200

    allow CORS

--- webapp/intro-to-data-sci/backend/app.py
+++ webapp/intro-to-data-sci/backend/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from model import get_prediction
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
+@cross_origin()
 def predict():
     return {"prediction": get_prediction()}
 
