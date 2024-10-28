665a62a365df81ec18d04897ae238166d1f06c33
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2024-10-28 22:20:26 +0200

    fix data path

--- webapp/intro-to-data-sci/backend/model.py
+++ webapp/intro-to-data-sci/backend/model.py
@@ -7,7 +7,7 @@ def get_prediction(pop_total,vaccine_coverage,visits_private,drug_offences,alcoh
                   "alcohol_sales", "combined_employed", "combined_higher_education",
                   "combined_0_18", "combined_18_64", "combined_65", "health_cost"]
     
-    df = pd.read_csv("../../data/data_clean.csv")[cols]
+    df = pd.read_csv("../../../data/data_clean.csv")[cols]
 
     X = df.iloc[:, :-1]
     y = df.iloc[:, -1]
@@ -17,4 +17,4 @@ def get_prediction(pop_total,vaccine_coverage,visits_private,drug_offences,alcoh
     # Predictions
     y_pred = lasso_cv.predict(np.array([pop_total,vaccine_coverage,visits_private,drug_offences,alcohol_sales,combined_employed,combined_higher_education,combined_0_18,combined_18_64, combined_65]))
 
-    return y_pred[0]
+    return 
