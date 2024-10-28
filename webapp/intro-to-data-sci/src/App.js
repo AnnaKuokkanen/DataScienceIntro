6f3c37bfb5ae560ed963d9c92bdf5291d399d687
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2024-10-28 11:19:10 +0200

    Add new fields

--- webapp/intro-to-data-sci/src/App.js
+++ webapp/intro-to-data-sci/src/App.js
@@ -27,9 +27,60 @@ function App() {
     <div className="App">
       <header className="header">
         <span>Welcome to healthcare cost predictor!</span>
+        <p></p>
+        <span>Please fill out the form to get your prediction</span>
       </header>
       <Form onSubmit={getPrediction}>
-        <Button type="submit">Predict healthcare costs</Button>
+        <Form.Group>
+          <Form.Label>Total population</Form.Label>
+          <Form.Control type="number" placeholder="population" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Higher education qualifications (% women)</Form.Label>
+          <Form.Control type="number" placeholder="Higher education qualifications (% women)" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Higher education qualifications (% men)</Form.Label>
+          <Form.Control type="number" placeholder="Higher education qualifications (% men)" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Employed (% women)</Form.Label>
+          <Form.Control type="text" placeholder="population" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Employed (% men)</Form.Label>
+          <Form.Control type="text" placeholder="population" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Flu vaccination coverage (% of population aged )</Form.Label>
+          <Form.Control type="text" placeholder="population" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Alcohol sales (litres in a year)</Form.Label>
+          <Form.Control type="text" placeholder="population" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Total population:</Form.Label>
+          <Form.Control type="text" placeholder="population" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Total population:</Form.Label>
+          <Form.Control type="text" placeholder="population" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Total population:</Form.Label>
+          <Form.Control type="text" placeholder="population" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Total population:</Form.Label>
+          <Form.Control type="text" placeholder="population" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Total population:</Form.Label>
+          <Form.Control type="text" placeholder="population" />
+        </Form.Group>
+
+        <Button class="button" type="submit">Predict healthcare costs</Button>
       </Form>
       {showPrediction && <span>Your prediction is {prediction}!</span>}
     </div>
