1a65d8ccfb9aac3cff2ff937c6a9035baf18f9fd
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2024-10-28 21:13:59 +0200

    style webapp

--- webapp/intro-to-data-sci/backend/model.py
+++ webapp/intro-to-data-sci/backend/model.py
@@ -1,2 +1,6 @@
+import pandas as pd
+import numpy as np
+
 def get_prediction():
+    df = pd.read_csv("../../data/")
     return 111
--- webapp/intro-to-data-sci/src/App.js
+++ webapp/intro-to-data-sci/src/App.js
@@ -27,59 +27,53 @@ function App() {
     <div className="App">
       <header className="header">
         <span>Welcome to healthcare cost predictor!</span>
-        <p></p>
-        <span>Please fill out the form to get your prediction</span>
       </header>
+      <span>Please fill out the form to get your prediction</span>
       <Form className="form" onSubmit={getPrediction}>
-        <Form.Group>
-          <Form.Label>Total population</Form.Label>
-          <Form.Control type="number" placeholder="population" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Higher education qualifications (% women)</Form.Label>
-          <Form.Control type="number" placeholder="Higher education qualifications (% women)" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Higher education qualifications (% men)</Form.Label>
-          <Form.Control type="number" placeholder="Higher education qualifications (% men)" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Employed (% women)</Form.Label>
-          <Form.Control type="text" placeholder="population" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Employed (% men)</Form.Label>
-          <Form.Control type="text" placeholder="population" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Flu vaccination coverage (% of population aged )</Form.Label>
-          <Form.Control type="text" placeholder="population" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Alcohol sales (litres in a year)</Form.Label>
-          <Form.Control type="text" placeholder="population" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Total population:</Form.Label>
-          <Form.Control type="text" placeholder="population" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Total population:</Form.Label>
-          <Form.Control type="text" placeholder="population" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Total population:</Form.Label>
-          <Form.Control type="text" placeholder="population" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Total population:</Form.Label>
-          <Form.Control type="text" placeholder="population" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Total population:</Form.Label>
-          <Form.Control type="text" placeholder="population" />
-        </Form.Group>
-
+        <div className='left'>
+          <Form.Group>
+            <Form.Label>Total population</Form.Label>
+            <Form.Control type="number"/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Higher education qualifications (% population)</Form.Label>
+            <Form.Control type="number"/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Employed (% population)</Form.Label>
+            <Form.Control type="number"/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Flu vaccination coverage (% of population aged 65 and over)</Form.Label>
+            <Form.Control type="number"/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Alcohol sales (litres in a year)</Form.Label>
+            <Form.Control type="number"/>
+          </Form.Group>
+        </div>
+        <div className='right'>
+          <Form.Group>
+            <Form.Label>Drug offences (per 1000 inhabitants)</Form.Label>
+            <Form.Control type="number"/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Visits to private healthcare services</Form.Label>
+            <Form.Control type="number"/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Population aged 0 to 18 (% of total population)</Form.Label>
+            <Form.Control type="number"/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Population aged 18 to 64 (% of total population)</Form.Label>
+            <Form.Control type="number"/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Population aged 65 and over (% of total population)</Form.Label>
+            <Form.Control type="number"/>
+          </Form.Group>
+        </div>
         <Button className="button" type="submit">Predict healthcare costs</Button>
       </Form>
       {showPrediction && <span>Your prediction is {prediction}!</span>}
