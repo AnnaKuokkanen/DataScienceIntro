bf162e8e504d80d246163b5146b4143652f74343
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2024-10-28 21:37:12 +0200

    more styling

--- webapp/intro-to-data-sci/src/App.js
+++ webapp/intro-to-data-sci/src/App.js
@@ -30,51 +30,53 @@ function App() {
       </header>
       <span>Please fill out the form to get your prediction</span>
       <Form className="form" onSubmit={getPrediction}>
-        <div className='left'>
-          <Form.Group>
-            <Form.Label>Total population</Form.Label>
-            <Form.Control type="number"/>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Higher education qualifications (% population)</Form.Label>
-            <Form.Control type="number"/>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Employed (% population)</Form.Label>
-            <Form.Control type="number"/>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Flu vaccination coverage (% of population aged 65 and over)</Form.Label>
-            <Form.Control type="number"/>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Alcohol sales (litres in a year)</Form.Label>
-            <Form.Control type="number"/>
-          </Form.Group>
+        <div className="field-container">
+          <div className='left'>
+            <Form.Group>
+              <Form.Label>Total population</Form.Label>
+              <Form.Control type="number"/>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Higher education qualifications (% population)</Form.Label>
+              <Form.Control type="number"/>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Employed (% population)</Form.Label>
+              <Form.Control type="number"/>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Flu vaccination coverage (% of population aged 65 and over)</Form.Label>
+              <Form.Control type="number"/>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Alcohol sales (litres in a year)</Form.Label>
+              <Form.Control type="number"/>
+            </Form.Group>
+          </div>
+          <div className='right'>
+            <Form.Group>
+              <Form.Label>Drug offences (per 1000 inhabitants)</Form.Label>
+              <Form.Control type="number"/>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Visits to private healthcare services</Form.Label>
+              <Form.Control type="number"/>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Population aged 0 to 18 (% of total population)</Form.Label>
+              <Form.Control type="number"/>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Population aged 18 to 64 (% of total population)</Form.Label>
+              <Form.Control type="number"/>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Population aged 65 and over (% of total population)</Form.Label>
+              <Form.Control type="number"/>
+            </Form.Group>
+          </div>
         </div>
-        <div className='right'>
-          <Form.Group>
-            <Form.Label>Drug offences (per 1000 inhabitants)</Form.Label>
-            <Form.Control type="number"/>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Visits to private healthcare services</Form.Label>
-            <Form.Control type="number"/>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Population aged 0 to 18 (% of total population)</Form.Label>
-            <Form.Control type="number"/>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Population aged 18 to 64 (% of total population)</Form.Label>
-            <Form.Control type="number"/>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Population aged 65 and over (% of total population)</Form.Label>
-            <Form.Control type="number"/>
-          </Form.Group>
-        </div>
-        <Button className="button" type="submit">Predict healthcare costs</Button>
+        <Button className="custom-button" type="submit" size="lg">Predict healthcare costs</Button>
       </Form>
       {showPrediction && <span>Your prediction is {prediction}!</span>}
     </div>
