f2da6689f44174bb03d27a315a1af767b7ff4638
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2024-10-28 23:03:50 +0200

    more styling and parameters

--- webapp/intro-to-data-sci/src/App.js
+++ webapp/intro-to-data-sci/src/App.js
@@ -9,11 +9,24 @@ function App() {
   const [prediction, setPrediction] = useState(0)
   const [showPrediction, setShowPrediction] = useState(false)
 
+
   function getPrediction(event) {
     event.preventDefault();
     axios({
       method: "GET",
-      url:"https://datascienceintro.onrender.com/",
+      url: "https://datascienceintro.onrender.com/",
+      params: {
+        pop_total: 100,
+        vaccine_coverage: 50,
+        visits_private: 243,
+        drug_offences: 4.5,
+        alcohol_sales: 28,
+        combined_employed: 60,
+        combined_higher_education: 80,
+        combined_0_18: 22,
+        combined_18_64: 62,
+        combined_65: 16
+      }
     }).then((response) => {
       const res = response.data
       setPrediction(res.prediction)
@@ -28,57 +41,67 @@ function App() {
       <header className="header">
         <span>Welcome to healthcare cost predictor!</span>
       </header>
-      <span>Please fill out the form to get your prediction</span>
-      <Form className="form" onSubmit={getPrediction}>
-        <div className="field-container">
-          <div className='left'>
-            <Form.Group>
-              <Form.Label>Total population</Form.Label>
-              <Form.Control type="number"/>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Higher education qualifications (% population)</Form.Label>
-              <Form.Control type="number"/>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Employed (% population)</Form.Label>
-              <Form.Control type="number"/>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Flu vaccination coverage (% of population aged 65 and over)</Form.Label>
-              <Form.Control type="number"/>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Alcohol sales (litres in a year)</Form.Label>
-              <Form.Control type="number"/>
-            </Form.Group>
+      {!showPrediction &&
+        <>
+          <span>Please fill out the form to get your prediction</span>
+          <Form className="form" onSubmit={getPrediction}>
+            <div className="field-container">
+              <div className='left'>
+                <Form.Group>
+                  <Form.Label>Total population</Form.Label>
+                  <Form.Control type="number"/>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Higher education qualifications (% population)</Form.Label>
+                  <Form.Control type="number"/>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Employed (% population)</Form.Label>
+                  <Form.Control type="number"/>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Flu vaccination coverage (% of population aged 65 and over)</Form.Label>
+                  <Form.Control type="number"/>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Alcohol sales (litres in a year)</Form.Label>
+                  <Form.Control type="number"/>
+                </Form.Group>
+              </div>
+              <div className='right'>
+                <Form.Group>
+                  <Form.Label>Drug offences (per 1000 inhabitants)</Form.Label>
+                  <Form.Control type="number"/>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Visits to private healthcare services</Form.Label>
+                  <Form.Control type="number"/>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Population aged 0 to 18 (% of total population)</Form.Label>
+                  <Form.Control type="number"/>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Population aged 18 to 64 (% of total population)</Form.Label>
+                  <Form.Control type="number"/>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Population aged 65 and over (% of total population)</Form.Label>
+                  <Form.Control type="number"/>
+                </Form.Group>
+              </div>
+            </div>
+            <Button className="custom-button" type="submit" size="lg">Predict healthcare costs</Button>
+          </Form>
+        </>}
+      {showPrediction &&
+        <>
+          <div>
+            <span>Your prediction is {prediction}!</span>
           </div>
-          <div className='right'>
-            <Form.Group>
-              <Form.Label>Drug offences (per 1000 inhabitants)</Form.Label>
-              <Form.Control type="number"/>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Visits to private healthcare services</Form.Label>
-              <Form.Control type="number"/>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Population aged 0 to 18 (% of total population)</Form.Label>
-              <Form.Control type="number"/>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Population aged 18 to 64 (% of total population)</Form.Label>
-              <Form.Control type="number"/>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Population aged 65 and over (% of total population)</Form.Label>
-              <Form.Control type="number"/>
-            </Form.Group>
-          </div>
-        </div>
-        <Button className="custom-button" type="submit" size="lg">Predict healthcare costs</Button>
-      </Form>
-      {showPrediction && <span>Your prediction is {prediction}!</span>}
+          <Button className="custom-button" size="sm" onClick={() => setShowPrediction(false)}>Try again</Button>
+        </>
+      }
     </div>
   );
 }
